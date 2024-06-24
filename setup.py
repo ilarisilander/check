d480e8ab6980f34b06e5b2abc23dce79083e9d2c
Ilari Silander <ilarisilander@hotmail.com>
2024-06-24 20:00:30 +0200

    Change version to 0.1.1-beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='check',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where='.'),
     py_modules=['todo'],
     install_requires=[
